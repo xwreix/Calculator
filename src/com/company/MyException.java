b04ed89da42336c25b6c70115c342ca8cf3eeaa1
xwreix <anastasya.kovaliova2016>
2020-10-25 20:28:47 +0300

    fix

--- src/com/company/MyException.java
+++ src/com/company/MyException.java
@@ -1,7 +1,7 @@
 package com.company;
-//halo
-public class NotNumberException extends Exception {
-    public NotNumberException(String message) {
+
+public class MyException extends Exception {
+    public MyException(String message) {
         super(message);
     }
 
--- src/com/company/UserInterface.java
+++ src/com/company/UserInterface.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 
 public class UserInterface {
 
-    private static ArrayList<String> history = new ArrayList<String>();
+    private static final ArrayList<String> history = new ArrayList<String>();
+    private static double number1;
+    private static double number2;
 
     public static void menu() {
         Scanner in = new Scanner(System.in);
@@ -16,92 +18,151 @@ public class UserInterface {
             System.out.println("Press 1 to calculate or 0 to see the history");
             choice = in.nextInt();
             in.nextLine();
+
             if (choice == 1) {
                 System.out.println("Choose the operation sign(+, -, *, /, ^, sqrt):");
                 String operation = in.nextLine();
                 operationSelection(operation);
-            } else if (choice == 0)
+            } else if (choice == 0) {
                 System.out.println(history);
-            else
+            } else {
                 System.out.println("You entered a wrong key");
-            System.out.println("Do you wanna continue? (1-yes, 0 - no)");
+            }
+
+            System.out.println("Press 1 to continue");
             key = in.nextInt();
+
         }
+
         in.close();
     }
 
-    public static void operationSelection(String operation) {
-
+    public static boolean checkFirstNumber(){
         Scanner in = new Scanner(System.in);
+        System.out.println("The number: ");
+        String num = in.nextLine();
+
+        try {
+            number1 = Double.parseDouble(num);
+            return true;
+        } catch (NumberFormatException ex) {
+            System.out.println("This element isn't a number");
+            history.add(num + " isn't a number");
+            return false;
+        }
+
+    }
+
+    public static  boolean checkSecondNumber(String operation){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Another one: ");
+        String num = in.nextLine();
+
+        try {
+            number2 = Double.parseDouble(num);
+            return true;
+        } catch (NumberFormatException ex) {
+            System.out.println("This element isn't a number");
+            history.add(num + " isn't a number");
+            return false;
+        }
+
+    }
+
+    public static void operationSelection(String operation) {
+        String result = "";
 
         switch (operation) {
             case "+":
-            case "-":
-            case "*":
-            case "^":
-            case "/":
-                System.out.println("The first number: ");
-                String number1 = in.nextLine();
-                System.out.println("The second number: ");
-                String number2 = in.nextLine();
 
-                try {
-                    Calculator.dataStandardInput(number1, number2);
-                } catch (NotNumberException ex) {
-                    System.out.println(ex.getMessage());
-                    history.add(number1 + operation + number2 + "=" + ex.getMessage());
+                if(!checkFirstNumber()) {
                     break;
                 }
 
-                double num1, num2;
-                num1 = Double.parseDouble(number1);
-                num2 = Double.parseDouble(number2);
-                String result = "";
-
-                switch (operation) {
-                    case "+":
-                        result = Calculator.addition(num1, num2);
-                        break;
-                    case "-":
-                        result = Calculator.subtraction(num1, num2);
-                        break;
-                    case "*":
-                        result = Calculator.multiplication(num1, num2);
-                        break;
-                    case "^":
-                        result = Calculator.exponentiation(num1, num2);
-                        break;
-                    case "/":
-                        try {
-                            Calculator.divisionCheck(num2);
-                            result = Calculator.division(num1, num2);
-                        } catch (ArithmeticException ex) {
-                            System.out.println(ex.getMessage());
-                            result = number1 + "/" + number2 + "=" + ex.getMessage();
-                        }
-                        break;
+                if(!checkSecondNumber(operation)) {
+                    break;
                 }
+
+                result = Calculator.addition(number1, number2);
+                System.out.println(result);
                 history.add(result);
                 break;
-            case "sqrt":
-                System.out.println("Enter the number: ");
-                String number = in.nextLine();
-                try {
-                    Calculator.sqrtCheck(number);
-                } catch (NotNumberException ex) {
-                    System.out.println(ex.getMessage());
-                    history.add("sqrt of"+number+"="+ex.getMessage());
-                    break;
-                } catch (ArithmeticException ex) {
-                    System.out.println(ex.getMessage());
-                    history.add("sqrt of "+number+"="+ex.getMessage());
+
+            case "-":
+
+                if(!checkFirstNumber()) {
                     break;
                 }
-                history.add(Calculator.sqrt(number));
+
+                if(!checkSecondNumber(operation)) {
+                    break;
+                }
+
+                result = Calculator.subtraction(number1, number2);
+                System.out.println(result);
+                history.add(result);
                 break;
+
+            case "*":
+
+                if(!checkFirstNumber()) {
+                    break;
+                }
+
+                if(!checkSecondNumber(operation)) {
+                    break;
+                }
+
+                result = Calculator.multiplication(number1, number2);
+                System.out.println(result);
+                history.add(result);
+                break;
+
+            case "^":
+
+                if(!checkFirstNumber()) {
+                    break;
+                }
+
+                if(!checkSecondNumber(operation)) {
+                    break;
+                }
+
+                result = Calculator.exponentiation(number1, number2);
+                System.out.println(result);
+                history.add(result);
+                break;
+
+            case "/":
+
+                if(!checkFirstNumber()) {
+                    break;
+                }
+
+                if(!checkSecondNumber(operation)) {
+                    break;
+                }
+
+                result = Calculator.division(number1, number2);
+                System.out.println(result);
+                history.add(result);
+                break;
+
+            case "sqrt":
+
+                if(!checkFirstNumber()) {
+                    break;
+                }
+
+                result = Calculator.sqrt(number1);
+                System.out.println(result);
+                history.add(result);
+                break;
+
             default:
                 System.out.println("You entered a wrong operation sign");
                 break;
+
         }
     }
 }
